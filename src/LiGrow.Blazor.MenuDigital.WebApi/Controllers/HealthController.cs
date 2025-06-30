63f6fc468af6e3f0a97fc7757e4e35bbdb44658d
ifredamg <fsraen@gmail.com>
2025-06-30 21:56:09 +0100

    Correções para publish

---
+++ src/LiGrow.Blazor.MenuDigital.WebApi/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using LiGrow.Blazor.MenuDigital.WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiGrow.Blazor.MenuDigital.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public bool Get()
+        {
+            return true;
+        }
+    }
+}
